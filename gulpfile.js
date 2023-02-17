7c7fc9ecce0fd9b1cdef11887e5883b44972e6f1
kardachh <kardakov.denis49@gmail.com>
2023-02-17 14:10:02 +0300

    123

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ const pug = require('pug');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const fs = require('fs');
+const beautify = require('gulp-beautify');
 
 const compile = function(){
     const template = pug.renderFile("./build/pug/index.pug")
     // console.log(template)
-    fs.writeFileSync('index.html', template)
+    fs.writeFileSync('./index.html', template)
 }
 
 function buildStyles() {
@@ -20,6 +21,13 @@ async function defaultTask() {
     await compile()
 }
 
+gulp.task('beautify-html', function() {
+    return gulp
+      .src('./index.html')
+      .pipe(beautify.html({ indent_size: 2 }))
+      .pipe(gulp.dest('./'));
+  });
+
 gulp.task('compile', async ()=>{
     await buildStyles()
     await compile()
@@ -27,5 +35,5 @@ gulp.task('compile', async ()=>{
 
 gulp.task('watch', function() {
     // livereload.listen({start:true});
-    gulp.watch('build/**/*.*', gulp.series("compile"));
+    gulp.watch('build/**/*.*', gulp.series("compile","beautify-html"));
 });
